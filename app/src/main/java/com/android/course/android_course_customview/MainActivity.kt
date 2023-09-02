50b5e3c06638e7386e723fd02520abd39e186a79
Rodion <swed197@mail.ru>
2023-09-02 13:20:30 +0500

    fiexes + new logic

--- app/src/main/java/com/android/course/android_course_customview/MainActivity.kt
+++ app/src/main/java/com/android/course/android_course_customview/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.android.course.android_course_customview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttonUp: Button
+    private lateinit var buttonDown: Button
+    private lateinit var speedometer: Speedometer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        speedometer = findViewById(R.id.speedometer)
+        buttonUp = findViewById<Button?>(R.id.button_up).apply {
+            setOnClickListener {
+                speedometer.speedUp()
+            }
+        }
+        buttonDown = findViewById<Button?>(R.id.button_down).apply {
+            setOnClickListener {
+                speedometer.speedDown()
+            }
+        }
     }
 }
--- app/src/main/java/com/android/course/android_course_customview/Speedometer.kt
+++ app/src/main/java/com/android/course/android_course_customview/Speedometer.kt
@@ -1,15 +1,18 @@
 package com.android.course.android_course_customview
 
+import android.R.attr.radius
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 import androidx.annotation.ColorInt
 import androidx.core.content.ContextCompat
 import kotlin.math.cos
 import kotlin.math.sin
+
 
 class Speedometer(context: Context, attributeSet: AttributeSet?) : View(context, attributeSet) {
 
@@ -50,6 +53,20 @@ class Speedometer(context: Context, attributeSet: AttributeSet?) : View(context,
         strokeWidth = 0.02f
     }
 
+    private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        textSize = 1f
+        color = ContextCompat.getColor(context, R.color.orange)
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.LEFT
+    }
+
+    private val speedPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        color = ContextCompat.getColor(context, R.color.purple)
+    }
+
+    private val speedToDraw = ArrayDeque<Pair<String, Pair<Float, Float>>>()
+
     init {
         val typedArray = context.theme.obtainStyledAttributes(
             attributeSet,
@@ -75,8 +92,19 @@ class Speedometer(context: Context, attributeSet: AttributeSet?) : View(context,
 
     override fun onDraw(canvas: Canvas?) {
         drawBackground(canvas)
+        drawSpeed(canvas)
         drawScale(canvas)
         drawArrow(canvas)
+    }
+
+    fun speedUp() {
+        speed += 1
+        invalidate()
+    }
+
+    fun speedDown() {
+        speed -= 1
+        invalidate()
     }
 
     private fun drawBackground(canvas: Canvas?) {
@@ -89,12 +117,15 @@ class Speedometer(context: Context, attributeSet: AttributeSet?) : View(context,
                     style = Paint.Style.FILL
                     color = ContextCompat.getColor(context, R.color.lightGrey)
                 })
+            save()
         }
     }
 
     private fun drawScale(canvas: Canvas?) {
         val scaleStep = Math.PI / maxSpeed
         val scaleLength = 0.9f
+        val halfCircumference = radius * Math.PI
+
 
         for (i in 0..maxSpeed) {
             val x1 = cos(Math.PI - scaleStep * i).toFloat()
@@ -103,7 +134,18 @@ class Speedometer(context: Context, attributeSet: AttributeSet?) : View(context,
                 val x2 = x1 * scaleLength * 0.9f
                 val y2 = y1 * scaleLength * 0.9f
                 canvas?.drawLine(x1, y1, x2, y2, bigScalePaint)
+                speedToDraw.addFirst(Pair("$i", Pair(x2, y2)))
+//                drawText(canvas)
             } else {
+
+                if (i < maxSpeed / 100 * 40)
+                    scalePaint.color = ContextCompat.getColor(context, R.color.green)
+                else if (i < maxSpeed / 100 * 80) {
+                    scalePaint.color = ContextCompat.getColor(context, R.color.yellow)
+                } else {
+                    scalePaint.color = ContextCompat.getColor(context, R.color.red)
+                }
+
                 val x2 = x1 * scaleLength
                 val y2 = y1 * scaleLength
                 canvas?.drawLine(x1, y1, x2, y2, scalePaint)
@@ -113,16 +155,34 @@ class Speedometer(context: Context, attributeSet: AttributeSet?) : View(context,
 
     private fun drawArrow(canvas: Canvas?) {
         canvas?.apply {
-            //ToDo Подумать
             rotate(90 - 180f * (speed.toFloat() / maxSpeed.toFloat()))
-
             drawLine(0f, 0f, 0f, 1f, arrowPaint)
-            //ToDo Check
-//            drawLine(-0.01f, 0f, 0f, 1f, arrowPaint)
             drawCircle(0f, 0f, 0.05f,
                 Paint(Paint.ANTI_ALIAS_FLAG).apply {
                     color = ContextCompat.getColor(context, R.color.orange)
                 })
+        }
+    }
+
+//    private fun drawText(canvas: Canvas?) {
+//        canvas?.apply {
+////            rotate(90f)
+//            for (i in speedToDraw) {
+//                println(i.second.first.toString() + i.second.second.toString())
+//                drawText(i.first, i.second.first, i.second.second, textPaint)
+//            }
+//        }
+//    }
+
+    private fun drawSpeed(canvas: Canvas?) {
+        canvas?.apply {
+            save()
+            translate(width.toFloat() / 2, 0f)
+            rotate(180f)
+            drawRect(-0.3f, -0.3f, 0.3f, 0f, speedPaint)
+            drawText("11", 0.01f, -0.01f, textPaint)
+            rotate(180f)
+            restore()
         }
     }
 }
